20c368787d377f525f03ba284c231b0eca6ba418
mariorodven <mariorodven@gmail.com>
2023-05-04 13:03:46 +0200

    proxy

--- pfpooAlumnos/src/fp2/poo/pfpNVT2454/Proxy.java
+++ pfpooAlumnos/src/fp2/poo/pfpNVT2454/Proxy.java
@@ -2,6 +2,20 @@ package fp2.poo.pfpNVT2454;
 
 import fp2.poo.utilidades.ProxyAbstracta;
 
-public class Proxy {
+public class Proxy extends ProxyAbstracta{
   
+  private List<URLBloqueadaInterfaz> urlbloqueadas = new ArrayList<URLBloqueadaInterfaz>();
+  
+  private List<SolicitudInterfaz> solicitudes = new ArrayList<SolicitudInterfaz> ();
+  
+  private List<RecursoLocalInterfaz> copiaLocal = new ArrayList<RecursoLocalInterfaz> ();
+
+  public Proxy(String nombreFicheroConf, String nombreFicheroSolicitudes)throws OperacionNoPermitidaExcepcion{
+    
+    LecturaConfiguracion lecturaConf = new LecturaConfiguracion(nombreFicheroConf);
+    
+    while (lecturaConf.hasNext()){
+      URLBloqueadaInterfaz urlBloqueda = lecturaConf.next( );
+      urlbloqueadas.add(urlBloqueda);
+    }
 }
